d2447834354c4d82e944c313edf25c007eee10b5
Artiom P <apoluyanovich@gmail.com>
2022-02-02 12:29:11 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Artiom Poluyanovich on 2.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
